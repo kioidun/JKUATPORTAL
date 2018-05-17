3506ca6ef56498738e1acddbe20da43ddb7d2c9a
Kioi <ianduncankioi24@gmail.com>
2018-05-17 16:05:11 +0300

    ongoing progress

--- app/src/main/java/com/example/kioi/jkuatportal/MainActivity.java
+++ app/src/main/java/com/example/kioi/jkuatportal/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.kioi.jkuatportal;
 
+import android.app.FragmentManager;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,10 +14,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.TextView;
-
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -131,7 +126,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main2, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -157,18 +152,24 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+            Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+            startActivity(intent);
+            finish();
 
         }
+        //else if (id == R.id.nav_gallery) {
+
+       // } else if (id == R.id.nav_slideshow) {
+
+
+
+        //} else if (id == R.id.nav_manage) {
+
+       // } else if (id == R.id.nav_share) {
+
+       // } else if (id == R.id.nav_send) {
+
+       // }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
--- app/src/main/java/com/example/kioi/jkuatportal/Timetable.java
+++ app/src/main/java/com/example/kioi/jkuatportal/Timetable.java
@@ -1,13 +1,54 @@
 package com.example.kioi.jkuatportal;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class Timetable extends AppCompatActivity {
+private Button mselectImage;
+private StorageReference mStorageReference;
+private static final int GALLERY_INTENT =2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timetable);
+
+        mStorageReference= FirebaseStorage.getInstance().getReference();
+        mselectImage=(Button)findViewById(R.id.selectImage);
+
+        mselectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(Intent.ACTION_PICK);
+                intent.setType("image");
+                startActivityForResult(intent,GALLERY_INTENT);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+
+        if(requestCode == GALLERY_INTENT && resultCode== RESULT_OK){
+            Uri uri = data.getData();
+            StorageReference filePath = mStorageReference.child("photos").child(uri.getLastPathSegment());
+
+            filePath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(Timetable.this,"upload Done",Toast.LENGTH_LONG).show();
+                }
+            });
+        }
     }
 }
